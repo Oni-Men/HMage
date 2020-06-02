c2499740333c27fcf1c4856f7ad4ca0074f49d7d
Namiken <namiken2525@yahoo.co.jp>
2020-06-03 01:04:25 +0900

    Capeの位置ずれのバグとプレイヤーが暗くなるバグを修正

--- src/main/java/onimen/anni/hmage/renderer/HMageRenderPlayer.java
+++ src/main/java/onimen/anni/hmage/renderer/HMageRenderPlayer.java
@@ -2,12 +2,15 @@ package onimen.anni.hmage.renderer;
 
 import java.util.Iterator;
 
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
 import net.minecraft.client.renderer.entity.layers.LayerCape;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.entity.EntityLivingBase;
 import onimen.anni.hmage.renderer.layer.HMageLayerBipedArmor;
+import onimen.anni.hmage.renderer.layer.HMageLayerCape;
 
 public class HMageRenderPlayer extends RenderPlayer {
 
@@ -25,13 +28,17 @@ public class HMageRenderPlayer extends RenderPlayer {
         iterator.remove();
         System.out.println("------------------REMOVE DEFAULT RENDERER----------:" + layerRenderer.getClass());
       } else if (layerRenderer instanceof LayerCape) {
-        //iterator.remove();
-        //System.out.println("------------------REMOVE DEFAULT RENDERER----------:" + layerRenderer.getClass());
+        iterator.remove();
+        System.out.println("------------------REMOVE DEFAULT RENDERER----------:" + layerRenderer.getClass());
       }
     }
 
-    //this.addLayer(new HMageLayerCape(this));
-    this.addLayer(new HMageLayerBipedArmor(this));
+    setLayer(0, new HMageLayerBipedArmor(this));
+    setLayer(4, new HMageLayerCape(this));
   }
 
+  @SuppressWarnings("unchecked")
+  public <V extends EntityLivingBase, U extends LayerRenderer<V>> void setLayer(int index, U layer) {
+    this.layerRenderers.set(index, (LayerRenderer<AbstractClientPlayer>) layer);
+  }
 }
