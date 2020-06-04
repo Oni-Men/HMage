ca62a6eff379859f2435034bbe3590cceb23dc6e
Namiken <namiken2525@yahoo.co.jp>
2020-06-05 00:00:59 +0900

    敵を殺す前に死んだ場合の処理を修正

--- src/main/java/onimen/anni/hmage/observer/data/GameInfo.java
+++ src/main/java/onimen/anni/hmage/observer/data/GameInfo.java
@@ -153,7 +153,7 @@ public class GameInfo {
       mePlayerData.incrementDeathCount();
     } else {
       //自身以外のデータを更新
-      AnniPlayerData countData = otherPlayerStatsMap.computeIfAbsent(dead, k -> new AnniPlayerData(k, killerTeam));
+      AnniPlayerData countData = otherPlayerStatsMap.computeIfAbsent(dead, k -> new AnniPlayerData(k, deadTeam));
       countData.incrementDeathCount();
     }
 
