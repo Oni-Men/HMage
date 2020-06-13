9a6a39066f6587242bf21792604243638404ced4
azure <rz7@outlook.jp>
2020-06-13 21:42:36 +0900

    Set usage handler

--- src/main/java/onimen/anni/hmage/command/NameCommand.java
+++ src/main/java/onimen/anni/hmage/command/NameCommand.java
@@ -13,9 +13,8 @@ import java.util.stream.Collectors;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
 
-import net.minecraft.command.CommandBase;
-import net.minecraft.command.CommandException;
-import net.minecraft.command.ICommandSender;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.command.*;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
@@ -44,7 +43,9 @@ public class NameCommand extends CommandBase implements IClientCommand {
 
   @Override
   public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-    if (args.length == 0) { return; }
+    if (args.length == 0) {
+      throw new WrongUsageException(getUsage(sender));
+    }
 
     executorService.execute(() -> {
       List<TextComponentString> messageList = new ArrayList<>();
