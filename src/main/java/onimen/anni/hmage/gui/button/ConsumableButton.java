2373e6b935c2db67bdddbe6f38c8b68ac82052c6
Oni-Men <sensyaheis@gmail.com>
2020-12-10 00:14:59 +0900

    ラムダ式を利用したボタンオブジェクト

---
+++ src/main/java/onimen/anni/hmage/gui/button/ConsumableButton.java
@@ -0,0 +1,41 @@
+package onimen.anni.hmage.gui.button;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import net.minecraft.client.gui.GuiButton;
+
+public class ConsumableButton implements ButtonObject {
+
+  private String title;
+  private String buttonText;
+  private List<String> description;
+  private Consumer<GuiButton> onClick;
+
+  public ConsumableButton(String title, String buttonText, Consumer<GuiButton> onClick) {
+    this.title = title;
+    this.buttonText = buttonText;
+    this.onClick = onClick;
+  }
+
+  @Override
+  public String getTitle() {
+    return this.title;
+  }
+
+  @Override
+  public String getButtonText() {
+    return this.buttonText;
+  }
+
+  @Override
+  public void actionPerformed(GuiButton button) {
+    this.onClick.accept(button);
+  }
+
+  @Override
+  public List<String> getDescription() {
+    return null;
+  }
+
+}
